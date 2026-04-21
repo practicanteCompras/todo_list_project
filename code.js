32a902c8db478fcbc412af0c2c89d63a2e7edda9
Robinson Mendoza <rmendozac@unal.edu.co>
2026-04-21 22:02:47 +0000

    Add a delete button with a basic style and a blank reset for the input.

--- code.js
+++ code.js
@@ -1,15 +1,29 @@
 function add_tasks(info){
-
     task = document.createElement("label")
     task_checkbox = document.createElement("input")
     task_checkbox.type = "checkbox"
     task_information = document.createTextNode(info)
-    document.body.append(task_checkbox)
-     document.body.append(task_information)
+    delete_button = document.createElement("button")
+    delete_button.style.backgroundColor = "red"
+    delete_button.style.padding = "8px"
+    document.body.appendChild(task_checkbox)
+    document.body.appendChild(task_information)
+    document.body.appendChild(delete_button)
+    delete_button.addEventListener("click", ()=>{
+        delete_task()
+})
+}
 
-};
 const input_information = document.getElementById("new_task")
 const btn = document.getElementById("add_task");
 btn.addEventListener("click", ()=>{
     add_tasks(input_information.value)
+    input_information.value = ""
 })
+
+
+function delete_task(){
+    task_checkbox.remove();
+    task_information.remove();
+    delete_button.remove()
+}
